c22bb496a8e5dbc7b1b21ce4085115b82339bcd5
Oliver Dahl <oliverpdahl@gmail.com>
2020-02-10 13:20:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    resp.write Time.new.hour >= 12 ? "Good Afternoon" : "Morning"
+    resp.write Time.new.hour >= 12 ? "Good Afternoon!" : "Good Morning!"
     resp.finish
   end
 end
